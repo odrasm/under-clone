e54c219f24a25915ee9562e9e40ce8a2d16d32b9
Federico Pinca <79964287+clash00@users.noreply.github.com>
2023-03-26 11:21:06 +0200

    Create github action unit test
    better tox for unit test
    test for launching init and firsts variables

--- tests/modules_test.sh
+++ tests/modules_test.sh
@@ -1,1 +1,7 @@
-echo -e '{\n"ANSIBLE_MODULE_ARGS": {\n"name":"BOOTSTRAP_CEPH",\n"match_mode":"prefix"\n}\n}' |jq | python3 ansible/library/find_disks.py
+# GITHUB WORKFLOW
+if [[ -n "$GITHUB_WORKSPACE" ]]; then
+  echo "We are running on github workflow for tox tests"
+  echo -e '{\n"ANSIBLE_MODULE_ARGS": {\n"name":"BOOTSTRAP_CEPH",\n"match_mode":"prefix"\n}\n}' | "${pythonLocation}/bin/jq" | python ansible/library/find_disks.py
+else
+  echo -e '{\n"ANSIBLE_MODULE_ARGS": {\n"name":"BOOTSTRAP_CEPH",\n"match_mode":"prefix"\n}\n}' | jq | python3 ansible/library/find_disks.py
+fi

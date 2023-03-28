2b254c1e9b35118b397d0219c953813789e9fb19
Federico Pinca <federico.pinca@e4company.com>
2023-03-29 01:08:08 +0200

    docker in docker test all-in-one e build to bootstrap

--- tests/underground_test.sh
+++ tests/underground_test.sh
@@ -3,3 +3,6 @@
 python -m pip install --upgrade --no-deps --force-reinstall /workspace/underground
 underground init aio zed -m minimal --inventory /workspace/underground/ansible/inventory/aio --configdir /workspace/etc/underground
 underground configure -e "@/workspace/underground/tests/test_vars.yml" --inventory /workspace/underground/ansible/inventory/aio --configdir /workspace/etc/underground
+underground build -t dib -e underground_baremetal_image="ipa" --inventory /workspace/underground/ansible/inventory/aio --configdir /workspace/etc/underground build
+underground bootstrap --inventory /workspace/underground/ansible/inventory/aio --configdir /workspace/etc/underground -e customize_etc_hosts=false
+
